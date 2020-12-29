2a8750568b92cd0743f8a58ab0ce8a5e0720d806
DerNoob24 <74051636+DerNoob24@users.noreply.github.com>
2020-12-29 13:07:57 +0100

    Update ampel.c

--- aufgabe_1/aufgabe_1_se/ampel/ampel.c
+++ aufgabe_1/aufgabe_1_se/ampel/ampel.c
@@ -1,10 +1,25 @@
 #include "../sharedLibs/sharedFunctions.h"
 
-int main(){
-    init(15,false);
-    while(1){
-        SET_BIT(PORTB,PORTB0);
-        SET_BIT(PORTB,PORTB1);
-        SET_BIT(PORTB,PORTB2);
+#define TOGGLE_BIT(PORT,BIT) PORT ^= (1<<BIT)
+
+void mainloop() {
+    TOGGLE_BIT(PORTB, PORTB0)
+    _delay_ms(1000)
+    TOGGLE_BIT(PORTB, PORTB1)
+    _delay_ms(500)
+    TOGGLE_BIT(PORTB, PORTB0)
+    TOGGLE_BIT(PORTB, PORTB1)
+    TOGGLE_BIT(PORTB, PORTB2)
+    _delay_ms(1000)
+    TOGGLE_BIT(PORTB, PORTB2)
+    TOGGLE_BIT(PORTB, PORTB1)
+    _delay_ms(500)
+    TOGGLE_BIT(PORTB, PORTB1)
+}
+
+int main() {
+    init(3, false);
+    while (1) {
+        mainloop()
     }
 }
