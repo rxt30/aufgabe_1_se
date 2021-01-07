5521a4d25754cf61e5525f7310ba2e5f01a6745c
rxt30 <mhampel@pm.me>
2021-01-07 12:55:45 +0100

    Refactoring

--- aufgabe_1/aufgabe_1_se/servomotor/servo.c
+++ aufgabe_1/aufgabe_1_se/servomotor/servo.c
@@ -1,4 +1,8 @@
 #include "../sharedLibs/sharedFunctions.h"
+/*
+ * OCR1A = 97 = engine on 0°
+ * OCR1A = 535 = engine on 180°
+ */
 ISR (INT0_vect){
 	OCR1A = 97;
 }
@@ -6,10 +10,9 @@ ISR (INT1_vect){
 	OCR1A = 535;
 }
 
-	void main(){
-		interruptInit();
-		 pwmOutputInit(4999,64);
-	
-		while (1){
-		}
+void main(){
+    interruptInit();
+	pwmOutputInit(4999,64);
+	while (1){
 	}
+}
